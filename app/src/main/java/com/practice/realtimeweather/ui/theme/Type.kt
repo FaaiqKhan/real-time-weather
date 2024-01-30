123cdba6e9e31b83ca95261f7325ef4573c1bafc
Faaiq Ali Khan <50907582+FaaiqKhan@users.noreply.github.com>
2024-01-30 12:13:52 +0100

    fonts added in application theme

--- app/src/main/java/com/practice/realtimeweather/ui/theme/Type.kt
+++ app/src/main/java/com/practice/realtimeweather/ui/theme/Type.kt
@@ -2,33 +2,126 @@ package com.practice.realtimeweather.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.*
 import androidx.compose.ui.unit.sp
+import com.practice.realtimeweather.R
+
+val Lora = FontFamily(
+    Font(R.font.lora_bold, FontWeight.Bold),
+    Font(R.font.lora_medium, FontWeight.Medium),
+    Font(R.font.lora_regular, FontWeight.Normal),
+    Font(R.font.lora_semi_bold, FontWeight.SemiBold),
+    Font(R.font.lora_italic, FontWeight.Thin),
+    Font(R.font.lora_bold_italic, FontWeight.Bold),
+    Font(R.font.lora_medium_italic, FontWeight.Medium),
+    Font(R.font.lora_semi_bold_italic, FontWeight.SemiBold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+    displayLarge = TextStyle(
+        fontSize = 20.sp,
+        fontFamily = Lora,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        letterSpacing = 0.5.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    displayMedium = TextStyle(
+        fontSize = 18.sp,
+        fontFamily = Lora,
+        lineHeight = 20.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    displaySmall = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Lora,
+        lineHeight = 20.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    headlineLarge = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Lora,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+    headlineMedium = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = Lora,
+        lineHeight = 20.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+    headlineSmall = TextStyle(
+        fontSize = 12.sp,
+        fontFamily = Lora,
+        lineHeight = 20.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 16.sp,
+        fontFamily = Lora,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    titleMedium = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = Lora,
+        lineHeight = 20.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    titleSmall = TextStyle(
+        fontSize = 12.sp,
+        fontFamily = Lora,
+        lineHeight = 20.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    bodyLarge = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Lora,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = Lora,
+        lineHeight = 22.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    bodySmall = TextStyle(
+        fontSize = 12.sp,
+        fontFamily = Lora,
+        lineHeight = 20.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    labelLarge = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Lora,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        fontWeight = FontWeight.Thin
+    ),
+    labelMedium = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = Lora,
+        lineHeight = 22.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.Thin
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 12.sp,
+        fontFamily = Lora,
+        lineHeight = 20.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.Thin
     )
-    */
 )
