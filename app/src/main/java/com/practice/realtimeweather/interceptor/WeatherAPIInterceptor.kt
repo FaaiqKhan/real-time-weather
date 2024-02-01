d6e2bc81e9fb411be31f28b6cd2353267b2bdf79
Faaiq Ali Khan <50907582+FaaiqKhan@users.noreply.github.com>
2024-02-01 16:27:57 +0100

    comment added in interceptor

--- app/src/main/java/com/practice/realtimeweather/interceptor/WeatherAPIInterceptor.kt
+++ app/src/main/java/com/practice/realtimeweather/interceptor/WeatherAPIInterceptor.kt
@@ -3,6 +3,11 @@ package com.practice.realtimeweather.interceptor
 import okhttp3.Interceptor
 import okhttp3.Response
 
+/**
+ * This interceptor is used to add apikey in the header before making call to the weather server.
+ * @param apiKey WeatherAPI key provided by the Weather website
+ */
+
 class WeatherAPIInterceptor(private val apiKey: String) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
